0e64b035dc101a99a0db022f87e873638f565b45
idoburnish <doyun7433@naver.com>
2021-07-30 00:05:07 +0900

    append http server

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const io = require('socket.io');
 const app = new express();
+const server = require("http").createServer(app);
+const io = require('socket.io')(server);
 
-var port = process.env.PORT || 8000;
-var server = app.listen(port, () => {
-    console.log("express server is ready.");
-})
+var port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log("express server is listening on port " + port);
+});
 
-const socketServer = io(server);
 
-socketServer.on("connection", function(socket) {
+io.on("connection", function(socket) {
     console.log("connect client " + socket.id);
+
+    socket.emit("example", "example from server");
 });
